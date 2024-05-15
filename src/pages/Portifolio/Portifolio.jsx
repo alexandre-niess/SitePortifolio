8e8d656a30e8a3ecc3b776412196ad8ea080b452
alexandre-niess <alexandrefersoa@gmail.com>
2024-05-15 14:08:57 -0300

    Adicionando titulo e descrição na página de portifólio e também colocando os botões dos perfis do GitHub e do Behance

--- src/pages/Portifolio/Portifolio.jsx
+++ src/pages/Portifolio/Portifolio.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useEffect } from "react";
-import { Typography, Box, Grid } from "@mui/material";
+import { Typography, Box, Grid, Button } from "@mui/material";
 import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import Navbar from "./Navbar";
 import GitHubCard from "./GitHubCard";
@@ -44,6 +44,20 @@ export function Portifolio() {
         "Algoritmo em Java que fazia requisições GET para verificar se o usuário preenchia os requisitos para receber um brinde da NuCommunity.",
     },
     {
+      languages: ["HTML", "CSS", "JavaScript"],
+      title: "Trabalho-Engenharia-I",
+      link: "https://github.com/alexandre-niess/Trabalho-Engenharia-I",
+      description:
+        "Este é um projeto desenvolvido como trabalho extra para a disciplina de Engenharia de Software I. O objetivo do projeto é simular um site de uma pizzaria fictícia chamada Freddy Fazbear's Pizza, proporcionando uma interface amigável e funcional.",
+    },
+    {
+      languages: ["HTML", "CSS", "JavaScript"],
+      title: "Malucão Sports",
+      link: "https://github.com/alexandre-niess/Malucao-Sports",
+      description:
+        "Este repositório contém o código-fonte do MVP (Produto Mínimo Viável) para a loja online Malucão Sports. O projeto foi desenvolvido para ajudar um amigo que cursa Administração na PUC Minas. Devido ao prazo apertado, optamos por usar um template pronto e adaptá-lo para nossas necessidades específicas.",
+    },
+    {
       languages: ["Markdown"],
       title: "Meu Perfil GitHub",
       link: "https://github.com/alexandre-niess/alexandre-niess",
@@ -58,7 +72,7 @@ export function Portifolio() {
         "Repositório com os códigos feitos na disciplina de Algoritmos e Estruturas de Dados II.",
     },
     {
-      languages: ["HTML", "JavaScript", "CSS"],
+      languages: ["HTML", "CSS", "JavaScript"],
       title: "Trabalho Final DIW",
       link: "https://github.com/alexandre-niess/Trabalho-2-DIW",
       description:
@@ -79,6 +93,21 @@ export function Portifolio() {
           alignItems: "center",
         }}
       >
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: "40px",
+          marginBottom: "20px",
+          textAlign: "center",
+        }}>
+          <Typography component="h2" variant="h4" sx={{ marginBottom: "10px" }}>
+            Portifólio
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Esse é o meu portifólio, onde eu concentro os meus principais projetos.
+          </Typography>
+        </Box>
         <ToggleButtonGroup
           sx={{ marginTop: "20px" }}
           color="primary"
@@ -92,52 +121,86 @@ export function Portifolio() {
         </ToggleButtonGroup>
 
         {alignment === "web" && (
-          <Box sx={{ flexGrow: 1, padding: 2, marginTop: "20px" }}>
-            <Grid container spacing={1}>
-              {cardData.map((data, index) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                  <GitHubCard {...data} />
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
+          <>
+            <Button
+              variant="outlined"
+              href="https://github.com/alexandre-niess"
+              color="secondary"
+              onClick={() => scrollToSection('contato')}
+              sx={{ borderRadius: '100px', marginTop: '40px' }}
+            >
+              Ver perfil no GitHub
+              <img
+                src="https://svgshare.com/i/167Y.svg"
+                alt="GitHub Icon"
+                style={{ marginLeft: '8px', width: '18px', height: 'auto' }}
+              />
+            </Button>
+            <Box sx={{ width: "100%", overflow: "hidden", marginTop: "40px" }}>
+
+              <Grid container spacing={1}>
+                {cardData.map((data, index) => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                    <GitHubCard {...data} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </>
         )}
 
         {alignment === "android" && (
-          <Box sx={{ width: "100%", overflow: "hidden", marginTop: "40px" }}>
-            <Grid container spacing={2}>
-              {[
-                "198581635",
-                "197649331",
-                "196564537",
-                "174655175",
-                "174654903",
-                "174654493",
-                "174654093",
-                "174653973",
-                "174653875",
-              ].map((id) => (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
-                  <iframe
-                    src={`https://www.behance.net/embed/project/${id}?ilo0=1`}
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      minHeight: "316px",
-                    }}
-                    allowFullScreen
-                    loading="eager"
-                    frameBorder="0"
-                    allow="clipboard-write"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                  ></iframe>
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
+          <>
+            <Button
+              href="https://www.behance.net/alexandrefersoa"
+              variant="outlined"
+              color="secondary"
+              onClick={() => scrollToSection('contato')}
+              sx={{ borderRadius: '100px', marginTop: '40px' }}
+            >
+              Ver perfil no Behance
+              <img
+                src="https://svgshare.com/i/167P.svg"
+                alt="Behance Icon"
+                style={{ marginLeft: '8px', width: '18px', height: 'auto' }}
+              />
+            </Button>
+            <Box sx={{ width: "100%", overflow: "hidden", marginTop: "40px" }}>
+
+              <Grid container spacing={2}>
+                {[
+                  "198581635",
+                  "197649331",
+                  "196564537",
+                  "174655175",
+                  "174654903",
+                  "174654493",
+                  "174654093",
+                  "174653973",
+                  "174653875",
+                ].map((id) => (
+                  <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
+                    <iframe
+                      src={`https://www.behance.net/embed/project/${id}?ilo0=1`}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        minHeight: "316px",
+                      }}
+                      allowFullScreen
+                      loading="eager"
+                      frameBorder="0"
+                      allow="clipboard-write"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                    ></iframe>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </>
         )}
         <Footer />
-      </Container>
+      </Container >
     </>
   );
 }
