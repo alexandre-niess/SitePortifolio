7fc65cf3ae07b578cf64fd892fc2e23ff3f36ef9
alexandre-niess <alexandrefersoa@gmail.com>
2024-05-17 17:53:01 -0300

    Atualizações

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { createRoot } from "react-dom/client";
 import { Portifolio } from "./pages/Portifolio/Portifolio.jsx";
 import { Home } from "./pages/Home/Home.jsx";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -16,6 +16,5 @@ ReactDOM.render(
         </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
