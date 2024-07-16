31f287d7706af2299ab9080e5b0fc11cfbf340b5
Alexandre Niess <alexandrefersoa@gmail.com>
2024-07-16 12:51:24 -0300

    Criação do AuthContext para permitir o acesso as rotas privadas

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+// src/context/AuthContext.jsx
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebaseConfig"; // Certifique-se de ajustar o caminho
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
