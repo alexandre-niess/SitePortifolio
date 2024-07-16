202616f164bba6bfe7cffd58b99af2459fcde208
Alexandre Niess <alexandrefersoa@gmail.com>
2024-07-16 12:42:53 -0300

    Ajustes nas rotas para funcionar na versão de deploy do firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,57 @@
+// src/main.jsx
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Portifolio } from "./pages/Portifolio/Portifolio.jsx";
 import { Home } from "./pages/Home/Home.jsx";
+import { Admin } from "./pages/Admin/Admin.jsx";
+import Login from "./pages/Admin/Login.jsx";
+import { AuthProvider } from "./context/AuthContext";
+import { PrivateRoute } from "./context/PrivateRoute";
+import ReactGA from "react-ga4";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#443399",
+    },
+    secondary: {
+      main: "#88AA55",
+    },
+    text: {
+      default: "#242424",
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+});
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/SitePortifolio" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="portifolio" element={<Portifolio />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portifolio />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <Admin />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </ThemeProvider>
 );
